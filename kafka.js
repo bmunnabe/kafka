8768482844c0c7ea8af5cbb66a72521c998031d1
bmunnabe <35695944+bmunnabe@users.noreply.github.com>
2021-09-14 20:06:31 +0530

    Create kafka.js

---
+++ kafka.js
@@ -0,0 +1,23 @@
+// Start ZooKeeper
+    .\bin\windows\kafka-server-start.bat .\config\zookeeper.properties
+
+
+// START Kafka Server
+    .\bin\windows\kafka-server-start.bat .\config\server.properties
+
+
+// Creating Topics
+    .\bin\windows\kafka-topics.bat --create --bootstrap-server 127.0.0.1:9092 --topic shifa
+   
+//  list/show all topics 
+    .\bin\windows\kafka-topics.bat --describe --zookeeper localhost:2181 --topic shifa
+    
+// Producer
+    .\bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic shifa
+  
+  
+// Listen to topic
+    .\bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic shifa
+    
+// Listen topic FROM BEGINNING (will listen if it already consumed by another consumer)
+    .\bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic shifa --from-beginning
